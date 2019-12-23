042ca21f3cacb631bc2e7151a8681f61acab8e9f
yusong <1193950650@qq.com>
2019-12-23 14:27:13 +0800

    add login pages

---
+++ lib/pages/login/index.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(219, 44, 31, 1),
+      //appBar: AppBar(title: Text('appbar')),
+      body: Container(child: Center(
+        child: Text('miu',style: TextStyle(color: Colors.white),),
+      )),
+    );
+  }
+}
