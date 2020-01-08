30105f2036e35543c64dc1636b01a12c8f82537f
yusong <1193950650@qq.com>
2020-01-08 18:55:33 +0800

    登录界面

--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -6,9 +6,64 @@ class Login extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromRGBO(219, 44, 31, 1),
       //appBar: AppBar(title: Text('appbar')),
-      body: Container(child: Center(
-        child: Text('miu',style: TextStyle(color: Colors.white),),
-      )),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: Center(
+                  child: Container(
+                    height: 75,
+                    width: 75,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(242, 42, 37, 1),
+                        borderRadius: BorderRadius.circular(100.0),
+                        image: DecorationImage(
+                            fit: BoxFit.none,
+                            image: AssetImage(
+                              'assets/images/login/login.png',
+                            ))),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Center(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(Radius.circular(25.0))
+                            ),
+                            child: Center(
+                              child: Text('手机号登录',style: TextStyle(color: Color.fromRGBO(255, 58, 58, 1))),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
